249fbbe1d508ddeeb09f352d05143f4ea008fff9
aaquinonez <alexisquionez21@gmail.com>
2024-04-15 16:26:48 -0300

    create index screen presentation

---
+++ app/login.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Page = () => {
+  return (
+    <View>
+      <Text>login</Text>
+    </View>
+  )
+}
+
+export default Page
+
+const styles = StyleSheet.create({})
